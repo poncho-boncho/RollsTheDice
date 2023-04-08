7a7ecb7f1f0083426882549732c66e290b4adb2a
poncho_boncho <Sorkolas@mail.ru>
2023-04-08 13:49:45 +0300

    added a button, displaying a number and projecting it to the activity at the click of a button

--- app/src/main/java/poncho_boncho/rollsthedice/MainActivity.kt
+++ app/src/main/java/poncho_boncho/rollsthedice/MainActivity.kt
@@ -1,17 +1,29 @@
 package poncho_boncho.rollsthedice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+
         val myFirstDice = Dice(6)
         val mySecondDice = Dice(20)
         println("your ${myFirstDice.numSides} sided dice rolled ${myFirstDice.roll()}")
         println("your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}")
 
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
     }
 }
